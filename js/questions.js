9c04cabeee7a32acd948265e6d32a11c93055bee
anudeep652 <anudeepsvka@gmail.com>
2021-08-04 18:35:17 +0530

    finish

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When was the first ICC T20 world Cup held?",
+    answer: "2007",
+    options: [
+      "2008",
+      "2006",
+      "2007",
+      "2009"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is the first team to win ICC Champions trophy",
+    answer: "South Africa",
+    options: [
+      "South Africa",
+      "Australia",
+      "India",
+      "Sri Lanka"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is the first man to score century in test cricket",
+    answer: "Charles Bannerman",
+    options: [
+      "Sachin Tendulkar",
+      "Sir Don Bradman",
+      "Charles Bannerman",
+      "Sir Vivian Richards"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is First man to score Double Century in ODI cricket",
+    answer: "Sachin Tendulkar",
+    options: [
+      "Rohit Sharma",
+      "Chris Gayle",
+      "Martin Guptil",
+      "Sachin Tendulkar"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who secure the Best T20i player of the decade 2010-2020 award",
+    answer: "Rashid Khan",
+    options: [
+      "Chris Gayle",
+      "Rashid Khan",
+      "Virat Kohli",
+      "Kiron Pollard"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
